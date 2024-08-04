6a0f1cd81954cfb32a0e1c1d3ec933df58507937
Denis Hamann <hamann.denis@gmail.com>
2024-08-05 01:25:20 +0200

    bump version

--- ArenaStats.lua
+++ ArenaStats.lua
@@ -357,7 +357,7 @@ function ArenaStats:DrawMinimapIcon()
                 end,
                 OnTooltipShow = function(tooltip)
                     tooltip:AddLine(string.format("%s |cff777777v%s|r", addonTitle,
-                        "0.2.4"))
+                        "0.2.5"))
                     tooltip:AddLine(string.format("|cFFCFCFCF%s|r %s", L["Left Click"],
                         L["to open the main window"]))
                     tooltip:AddLine(string.format("|cFFCFCFCF%s|r %s", L["Right Click"],
